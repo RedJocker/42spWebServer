c894b516f75ba3f94e14cf29e35a57dd400f5b3a
Victor Carrara Polido Loureiro <vcarrara@c2r3p1.42sp.org.br>
2025-08-26 12:00:14 -0300

    feat: make Request parser epoll-friendly with buffered reads and headers

--- src/http/Request.cpp
+++ src/http/Request.cpp
@@ -6,7 +6,7 @@
 /*   By: vcarrara <vcarrara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/25 10:51:33 by vcarrara          #+#    #+#             */
-/*   Updated: 2025/08/26 10:58:32 by vcarrara         ###   ########.fr       */
+/*   Updated: 2025/08/26 11:57:12 by vcarrara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,64 +14,77 @@
 #include <unistd.h>
 #include <sstream>
 #include <iostream>
+#include <cerrno>
 
 namespace http
 {
-	Request::Request(void) {
-	}
-
-	Request::Request(const Request &other) {
-		*this = other;
-	}
-
+	Request::Request(void) {}
+	Request::Request(const Request &other) { *this = other; }
 	Request &Request::operator=(const Request &other) {
 		if (this != &other) {
 			this->_method = other._method;
 			this->_path = other._path;
 			this->_protocol = other._protocol;
+			this->_headers = other._headers;
+			this->_readBuffer = other._readBuffer;
 		}
 		return *this;
 	}
+	Request::~Request(void) {}
 
-	Request::~Request(void) {
-	}
-
-	static bool	readLineFromFd(int fd, std::string &out) {
-		out.clear();
-		char ch = 0;
-		bool sawCR = false;
-
-		while (true) {
-			ssize_t	n = ::read(fd, &ch, 1);
-			if (n <= 0)
-				return false; // Error before EOL or EOF
-			if (ch == '\r') {
-				sawCR = true;
-				continue; // Expecting \r\n
-			}
-			if (ch == '\n')
-				return true; // EOL
-			if (sawCR) {
-				out.push_back(ch);
-				return true; // First line only
-			}
-			out.push_back(ch);
+	bool Request::readFromFd(int fd) {
+		char	tmp[4096];
+		ssize_t	n = ::read(fd, tmp, sizeof(tmp));
+		if (n == -1) {
+			if (errno == EAGAIN || errno == EWOULDBLOCK)
+				return false; // No data available
+			return false; // Error
 		}
-	}
+		if (n == 0)
+			return false; // EOF, connection closed
 
-	bool Request::parseFromFd(int fd) {
-		std::string requestLine;
-		if (!readLineFromFd(fd, requestLine))
-			return false; // EOF or error
+		_readBuffer.append(tmp, n);
 
-		std::istringstream rl(requestLine);
-		if (!(rl >> _method >> _path >> _protocol))
-			return false; // Failed to populate
+		size_t	pos = _readBuffer.find("\r\n\r\n");
+		if (pos == std::string::npos)
+			return false; // Incomplete request line or headers
 
-		return true;
+		std::istringstream stream(_readBuffer.substr(0, pos));
+		std::string line;
+
+		if (!std::getline(stream, line))
+			return false; // Failed to read request line
+		if (!line.empty() && line.back() == '\r')
+			line.pop_back(); // Remove trailing CR
+		std::istringstream lineStream(line);
+		if (!(lineStream >> _method >> _path >> _protocol))
+			return false; // Failed to parse request line
+
+		while (std::getline(stream, line)) {
+			if (!line.empty() && line.back() == '\r')
+				line.pop_back();
+			if (line.empty())
+				break; // End of headers
+
+			size_t colon = line.find(':');
+			if (colon != std::string::npos) {
+				std::string key = line.substr(0, colon);
+				std::string value = line.substr(colon + 1);
+				while (!value.empty() && value[0] == ' ')
+					value.erase(0, 1); // Trim leading spaces
+				_headers[key] = value;
+			}
+		}
+		_readBuffer.erase(0, pos + 4); // Remove processed part
+
+		return true; // Successfully parsed
 	}
 
 	std::string Request::getMethod() const { return _method; }
 	std::string Request::getPath() const { return _path; }
 	std::string Request::getProtocol() const { return _protocol; }
+	std::string Request::getHeader(const std::string &key) const {
+		std::map<std::string, std::string>::const_iterator it = _headers.find(key);
+		return it != _headers.end() ? it->second : "";
+	}
 }
--- src/http/Request.hpp
+++ src/http/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: vcarrara <vcarrara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/25 10:41:51 by vcarrara          #+#    #+#             */
-/*   Updated: 2025/08/26 10:47:31 by vcarrara         ###   ########.fr       */
+/*   Updated: 2025/08/26 11:28:33 by vcarrara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define REQUEST_HPP
 
 #include <string>
+#include <map>
 
 namespace http
 {
@@ -22,6 +23,9 @@ namespace http
 		std::string _method;
 		std::string _path;
 		std::string _protocol;
+		std::map<std::string, std::string> _headers;
+
+		std::string _readBuffer; // non-blocking
 
 	public:
 
@@ -30,11 +34,12 @@ namespace http
 		virtual Request &operator=(const Request &other);
 		virtual ~Request(void);
 
-		bool parseFromFd(int fd); // True if populated successfully
+		bool readFromFd(int fd); // True if populated successfully
 
 		std::string getMethod() const;
 		std::string getPath() const;
 		std::string getProtocol() const;
+		std::string getHeader(const std::string &key) const;
 	};
 }
 
